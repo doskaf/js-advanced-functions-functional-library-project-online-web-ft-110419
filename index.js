b4596851671ff26389ba0cf8ad5bcaae9609c950
Dominique Skaf <doskaf@yahoo.com>
2020-03-07 22:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -114,8 +114,7 @@ const fi = (function() {
     
     flatten: function(array, shallow) {
       if (shallow) {
-        let arr;
-        return arr = array.flat(1)
+        
       } else {
         return array.join().split(",").map(e => parseInt(e))
       }
