dde91946cb8552d162f2908c7ff2d5d5640746c3
Dominique Skaf <doskaf@yahoo.com>
2020-03-07 17:12:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fi = (function() {
 
     each: function(collection, callback) {
       for (let i = 0; i < collection.length; i++) {
-        callback(collection[i], collection[i][i], collection)
+        callback(collection[i], collection[i], collection)
       }
       return collection
     },
